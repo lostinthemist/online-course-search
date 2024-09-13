d4b9ec67a4dda5d4b4f23545c1e290148c6386f8
Rauno Kaldmaa <7577899+lostinthemist@users.noreply.github.com>
2024-09-13 03:15:34 +0300

    courses pages/api route setup

---
+++ src/pages/api/courses.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { OrgCourseListResponses } from '../../interfaces/course'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { offset = '0', count = '20', filter_conditions } = req.query
+
+    const response = await fetch(`https://api-rest.elice.io/org/academy/course/list/?offset=${offset}&count=${count}&filter_conditions=${encodeURIComponent(filter_conditions as string)}`)
+    const data: OrgCourseListResponses = await response.json()
+
+    console.log('Raw data fetched:', data.course_count)
+    console.log('Courses fetched:', data.courses)
+
+    if (!data?.courses) {
+      throw new Error('Invalid API response: "courses" field is missing')
+    }
+
+    res.status(200).json({ courses: data.courses, totalCourses: data.course_count })
+  } catch (error) {
+    console.error('Error fetching courses:', error)
+    res.status(500).json({ message: 'Failed to fetch courses' })
+  }
+}
